5811d3c425bd79dc2c66ec5c6712aee9f8fc5138
Luca Lezzerini <luca@lezzerini.it>
2019-07-04 14:51:06 +0200

    Secondo commit: server

---
+++ server/src/main/java/it/digidgroup/kezapp02/Kezapp02Application.java
@@ -0,0 +1,13 @@
+package it.digidgroup.kezapp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kezapp02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kezapp02Application.class, args);
+	}
+
+}
