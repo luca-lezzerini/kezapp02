fb1ed315429f24b1509b15cf8f3129be1d1de8bf
Luca Lezzerini <luca@lezzerini.it>
2019-07-04 17:59:22 +0200

    Interfaccia Service 0

--- server/src/main/java/it/digidgroup/kezapp02/dto/RichiediRegistrazioneDto.java
+++ server/src/main/java/it/digidgroup/kezapp02/dto/RichiediRegistrazioneDto.java
@@ -1,5 +1,14 @@
 package it.digidgroup.kezapp02.dto;
 
 public class RichiediRegistrazioneDto {
+    private String nickName;
 
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+    
 }
--- server/src/main/java/it/digidgroup/kezapp02/service/KeZappService0.java
+++ server/src/main/java/it/digidgroup/kezapp02/service/KeZappService0.java
@@ -1,5 +1,9 @@
 package it.digidgroup.kezapp02.service;
 
+import it.digidgroup.kezapp02.dto.RegistrazioneDto0;
+import it.digidgroup.kezapp02.dto.RichiediRegistrazioneDto;
+
 public interface KeZappService0 {
-    
+
+    RegistrazioneDto0 registrazione(RichiediRegistrazioneDto dto);
 }
