6aee22bba71de08e65bddca43a42526251bbe7a6
ccervini97 <cri.cervini@jimmycorporation.com>
2019-07-09 12:19:28 +0200

    prova

---
+++ client/kezapp02-client01/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  sessione: string;
+  nickName: string;
+
+
+  constructor(private http: HttpClient){}
+  registrazione(){
+let ox: Observa
+  }
+}
