d4dc0ad9e2530b00be58bc9aa6fbc816ddc9351c
Luca Lezzerini <luca@lezzerini.it>
2019-07-09 12:18:13 +0200

    Modificato nome cartella client

--- client/kezapp02-client0/src/app/app.component.ts
+++ client/kezapp02-client0/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kezapp02-client';
+  sessione: string;
+  nickName: string;
+
+  constructor(private http: HttpClient){}
+
+  registrazione(){
+    let ox: Observable<
+  }
 }
--- server/src/main/java/it/digidgroup/kezapp02/controller/KeZappController0.java
+++ server/src/main/java/it/digidgroup/kezapp02/controller/KeZappController0.java
@@ -1,5 +1,26 @@
 package it.digidgroup.kezapp02.controller;
 
+import it.digidgroup.kezapp02.dto.RegistrazioneDto0;
+import it.digidgroup.kezapp02.dto.RichiediRegistrazioneDto0;
+import it.digidgroup.kezapp02.service.KeZappService0;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
 public class KeZappController0 {
 
+    @Autowired
+    KeZappService0 ks;
+
+    @RequestMapping(value = {"/registrazione"})
+    @ResponseBody
+    RegistrazioneDto0 registrazione(
+            @RequestBody RichiediRegistrazioneDto0 dto) {
+        return ks.registrazione(dto);
+    }
 }
