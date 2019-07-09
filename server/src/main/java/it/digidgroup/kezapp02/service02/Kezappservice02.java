27db9b9f4c86697f4d0c16a7182bb08c383fd52c
Luca Lezzerini <luca@lezzerini.it>
2019-07-09 09:28:18 +0200

    Rimosso package errato

--- server/src/main/java/it/digidgroup/kezapp02/service02/Kezappservice02.java
+++
@@ -1,8 +0,0 @@
-package it.digidgroup.kezapp02.service02;
-
-import it.digidgroup.kezapp02.dto.RegistrazioneDto02;
-import it.digidgroup.kezapp02.dto.RichiediRegistrazioneDto02;
-
-public interface Kezappservice02 {
-    RegistrazioneDto02 registrazione(RichiediRegistrazioneDto02 dto);
-}
