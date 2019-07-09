e9f9496a8916c1f63950d69d344aa2b6a6a5ea94
ccervini97 <cri.cervini@jimmycorporation.com>
2019-07-09 10:26:41 +0200

    Vi ho inserito errori ahah

--- server/src/main/java/it/digidgroup/kezapp02/repository/ChatRepository01.java
+++ server/src/main/java/it/digidgroup/kezapp02/repository/ChatRepository01.java
@@ -1,11 +1,11 @@
 package it.digidgroup.kezapp02.repository;
 
-import it.digidgroup.kezapp02.model.Chat0;
+import it.digidgroup.kezapp02.model.Chat01;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRepository01 
-        extends JpaRepository<Chat0, Long>{
-    Chat0 findByNickname(String nickname);
+        extends JpaRepository<Chat01, Long>{
+    Chat01 findByNickname(String nickname);
 }
