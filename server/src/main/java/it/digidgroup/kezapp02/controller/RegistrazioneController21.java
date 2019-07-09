e1373ce563b7ec2df39dc952c347f1e928cccb7d
vic90s <simonaevangelisti90@gmail.com>
2019-07-09 12:18:47 +0200

    Modificato nome cartella client

--- server/src/main/java/it/digidgroup/kezapp02/controller/RegistrazioneController21.java
+++ server/src/main/java/it/digidgroup/kezapp02/controller/RegistrazioneController21.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.digidgroup.kezapp02.controller;
 
-/**
- *
- * @author utente
- */
+import it.digidgroup.kezapp02.dto.RegistrazioneDto21;
+import it.digidgroup.kezapp02.dto.RichiediRegistrazioneDto21;
+import it.digidgroup.kezapp02.service.RegistrazioneService21;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
 public class RegistrazioneController21 {
     
+    @Autowired
+    RegistrazioneService21 ks;
+    
+    @RequestMapping(value = {"/registrazione"})
+    @ResponseBody
+    RegistrazioneDto21 registrazione(@RequestBody RichiediRegistrazioneDto21 dto){
+        return ks.registrazione(dto);
+    }
 }
