e02cfc2eb05f478e9c5b3a5bc15c5d30acf4317f
sissichan <losardo.siria@gmail.com>
2019-07-11 15:54:32 +0200

    Implementato kezapp02

--- client/kezapp02-client3/src/app/app.component.ts
+++ client/kezapp02-client3/src/app/app.component.ts
@@ -20,19 +20,45 @@ export class AppComponent {
   constructor(private http: HttpClient){}
 
   registrazione(){
+    console.log("Siamo in registrazione");
     let ox: Observable<RegistrazioneDto> =
     this.http.post<RegistrazioneDto>
     ("http://localhost:8080/registrazione3",{nickName: this.nickName});
-    ox.subscribe( r => this.sessione = r.sessione);
+    ox.subscribe( r => {
+      this.sessione = r.sessione;
+      this.aggiorna();
+    });
   }
+
   inviaTutti(){
     let ox: Observable<RegistrazioneDto> =
     this.http.post<RegistrazioneDto>
     ("http://localhost:8080/inviaTutti3",
     {sessione: this.sessione, 
      messaggio: this.messaggio,
-    destinatario: null });
+     destinatario: null });
     ox.subscribe( r => {this.contatti = r.contatti,
                         this.messaggi = r.messaggi});
   }
+  
+  inviaUno(c: Chat){
+    let ox: Observable<RegistrazioneDto> =
+    this.http.post<RegistrazioneDto>
+    ("http://localhost:8080/inviaUno3",
+    {sessione: this.sessione, 
+     messaggio: this.messaggio,
+     destinatario: c.nickName });
+    ox.subscribe( r => {this.contatti = r.contatti,
+                        this.messaggi = r.messaggi});
+  }
+
+  aggiorna(){
+    let ox: Observable<RegistrazioneDto> =
+    this.http.post<RegistrazioneDto>
+    ("http://localhost:8080/aggiorna3",
+    {sessione: this.sessione});
+    ox.subscribe( r => {this.contatti = r.contatti,
+                        this.messaggi = r.messaggi});
+  }
+
 }
--- server/src/main/java/it/digidgroup/kezapp02/dto/RichiediMessaggioDto3.java
+++ server/src/main/java/it/digidgroup/kezapp02/dto/RichiediMessaggioDto3.java
@@ -1,5 +1,13 @@
 package it.digidgroup.kezapp02.dto;
 
 public class RichiediMessaggioDto3 {
-    
+    private String sessione;
+
+    public String getSessione() {
+        return sessione;
+    }
+
+    public void setSessione(String sessione) {
+        this.sessione = sessione;
+    }
 }
